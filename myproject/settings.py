46c8959db87d532c64161d57d3ee07e23672fd93
vknan <70138450+vknan@users.noreply.github.com>
2023-04-16 17:31:50 +0530

    allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*%#@vegjt5vdg)rg$h63ebp-3kyimzxb8=*%b%0cg8osa^z36c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vknan.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
