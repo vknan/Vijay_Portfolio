a8a7f298e9c8c2ddeef1d281403db0c44727510f
vknan <70138450+vknan@users.noreply.github.com>
2023-11-23 17:21:47 +0530

    Menu Bar update

--- myapp/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import PasswordResetForm
-
-class CustomPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
