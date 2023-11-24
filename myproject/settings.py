1630f450c42e705ff74d727f1918bd8cd62d5eb4
vknan <70138450+vknan@users.noreply.github.com>
2023-11-24 15:50:15 +0530

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*%#@vegjt5vdg)rg$h63ebp-3kyimzxb8=*%b%0cg8osa^z36c-^"
+SECRET_KEY = "django-insecure-*%#@vegjt5vdg_rg$h63ebp-3kyimzxb8=*%b%0cg8osa^z36c-^."
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vknan.pythonanywhere.com', '127.0.0.1']
 
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static'),]
 # print(STATICFILES_DIRS)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
