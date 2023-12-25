5647021409538de43967c68fda42a1f48fbdf372
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2023-12-25 15:23:33 +0530

    assets removed

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- myproject/urls.py
+++ myproject/urls.py
@@ -33,6 +33,10 @@ urlpatterns = [
 #     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+     urlpatterns+= static(settings.STATIC_URL)
+     urlpatterns+= static(settings.MEDIA_URL)
