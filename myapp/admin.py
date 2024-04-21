d94b3e5de1738eb5e8e78b8e49a9431b65d77276
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2024-04-21 21:17:30 +0530

    Update2

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
-from .models import Feature,Category,Post, Comment, Contact, FilesAdmin
-
-
-
-
-
-
-
+from .models import Category,Post, Comment, Contact, Course
 # # Unregister your User model
 # admin.site.unregister(User)
 # Register your models here.
-
-
-
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructor')
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'last_login')
@@ -33,7 +25,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('contacted_date', 'is_resolved')
     list_editable = ('is_resolved',)
 
-admin.site.register(Feature) 
+admin.site.register(Course, CourseAdmin) 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
@@ -41,30 +33,30 @@ admin.site.register(Contact, ContactAdmin)
 
 #------------------------------------------------------------------------------------------------
 
-from .models import ProductCategory, Product, Customer, OrderItem, Review
+# from .models import ProductCategory, Product, Customer, OrderItem, Review
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
+# class OrderItemInline(admin.TabularInline):
+#     model = OrderItem
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'product_category', 'price_actual', 'discounted_price', 'avg_rating']
-    list_editable = ['price_actual', 'discounted_price']
-    list_filter = ['product_category']
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'product_category', 'price_actual', 'discounted_price', 'avg_rating']
+#     list_editable = ['price_actual', 'discounted_price']
+#     list_filter = ['product_category']
 
-    inlines = [OrderItemInline]
+#     inlines = [OrderItemInline]
 
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['product', 'customer', 'ordered', 'date_added']
-    list_editable = ['ordered']
-    list_filter = ['date_added']
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_display = ['product', 'customer', 'ordered', 'date_added']
+#     list_editable = ['ordered']
+#     list_filter = ['date_added']
 
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['customer', 'product', 'rating', 'comment']
-    list_filter = ['customer', 'product']
+# class ReviewAdmin(admin.ModelAdmin):
+#     list_display = ['customer', 'product', 'rating', 'comment']
+#     list_filter = ['customer', 'product']
 
-admin.site.register(ProductCategory)
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Customer)
-admin.site.register(OrderItem, OrderItemAdmin)
-admin.site.register(Review, ReviewAdmin)
-admin.site.register(FilesAdmin)
+# admin.site.register(ProductCategory)
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Customer)
+# admin.site.register(OrderItem, OrderItemAdmin)
+# admin.site.register(Review, ReviewAdmin)
+# admin.site.register(FilesAdmin)
